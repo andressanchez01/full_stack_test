c85056b2f615af1bd3971ea55b1c4be9d9780b7d
Andres Sanchez <andres_sanchez01@hotmail.com>
2025-04-04 13:28:00 -0500

    transaction repository

---
+++ backend/app/repositories/transaction_repository.rb
@@ -0,0 +1,33 @@
+class TransactionRepository
+    def self.create(transaction_data)
+      transaction = Transaction.new(transaction_data)
+      
+      if transaction.save
+        Result.success(transaction)
+      else
+        Result.failure(transaction.errors.full_messages.join(", "))
+      end
+    end
+  
+    def self.find_by_id(id)
+      transaction = Transaction.find_by(id: id)
+      return Result.failure("Transaction not found") unless transaction
+      Result.success(transaction)
+    end
+  
+    def self.update_status(id, status, provider_transaction_id = nil)
+      result = find_by_id(id)
+      return result if result.failure?
+      
+      transaction = result.value
+      
+      update_data = { status: status }
+      update_data[:provider_transaction_id] = provider_transaction_id if provider_transaction_id
+      
+      if transaction.update(update_data)
+        Result.success(transaction)
+      else
+        Result.failure(transaction.errors.full_messages.join(", "))
+      end
+    end
+  end
